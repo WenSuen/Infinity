50efb37aa5df6d89b5dfbb9b6668752835dd6600
WenSuen <108275035+WenSuen@users.noreply.github.com>
2025-03-03 09:23:37 +0000

    added css and change header bg color to black

--- js/script.js
+++ js/script.js
@@ -63,7 +63,8 @@ function handleHeaderScroll() {
     // Check if the page is either the Products, About, or Contact page
     const isSolidHeaderPage = document.body.classList.contains('products-page') || 
                               document.body.classList.contains('about-page') ||
-                              document.body.classList.contains('contact-page');
+                              document.body.classList.contains('contact-page') ||
+                              document.body.classList.contains('gallery-page');
 
     if (!isSolidHeaderPage) {
         window.addEventListener('scroll', () => {
